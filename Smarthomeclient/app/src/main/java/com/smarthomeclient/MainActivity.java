b211af3529f751aec72ea9770da14446f261b93c
semihokanpehlivan <semihokanp@gmail.com>
2016-03-26 00:23:41 +0200

    Added valid IP address controlling

--- Smarthomeclient/app/src/main/java/com/smarthomeclient/MainActivity.java
+++ Smarthomeclient/app/src/main/java/com/smarthomeclient/MainActivity.java
@@ -8,20 +8,21 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import jodd.http.HttpRequest;
-import jodd.http.HttpResponse;
-
+/**
+ * Author semihokan.
+ */
 public class MainActivity extends AppCompatActivity {
 
-    Button openButton;
-    Button closeButton;
-    EditText ip;
+
+    EditText ipEditText;
+    private IPAddressValidator ipAddressValidator = new IPAddressValidator();
 
     private static final Logger logger = Logger.getLogger(MainActivity.class.getName());
 
@@ -39,9 +40,9 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-        ip = (EditText) findViewById(R.id.editText);
         //openButtonListener();
         //closeButtonListener();
+        setIPAddressListener();
     }
 
     @Override
@@ -64,6 +65,32 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    Button ipAddressButton;
+
+    private void setIPAddressListener() {
+        ipAddressButton = (Button) findViewById(R.id.ip_address);
+        ipAddressButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ipEditText = (EditText) findViewById(R.id.editText);
+                String ipAddress = ipEditText.getText().toString();
+                logger.info("Entered IP address: " + ipAddress);
+                boolean isValidIPAddress = ipAddressValidator.validate(ipAddress);
+                if (isValidIPAddress) {
+                    //TODO: assign the ip address to global variable for all activities
+                    logger.info("IP address is valid");
+                } else {
+                    logger.log(Level.INFO, "IP address is in valid");
+                }
+                Toast ipToast = Toast.makeText(getApplicationContext(), "Entered IP Address: ".concat(ipAddress+" " + String.valueOf(isValidIPAddress)), Toast.LENGTH_SHORT);
+                ipToast.setDuration(Toast.LENGTH_SHORT);
+                ipToast.show();
+            }
+        });
+
+
     }
 /*
     private void openButtonListener() {
