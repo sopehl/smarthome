94d47afa04757c17088acd7b406df961fe9145f3
semihokanpehlivan <semihokanp@gmail.com>
2016-04-28 09:24:02 +0300

    Added camera requests and jodd library

--- Smarthomeclient/app/src/main/java/com/smarthomeclient/CameraWebViewActivity.java
+++ Smarthomeclient/app/src/main/java/com/smarthomeclient/CameraWebViewActivity.java
@@ -2,8 +2,6 @@ package com.smarthomeclient;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -11,9 +9,14 @@ import android.webkit.WebView;
 
 import java.util.logging.Logger;
 
+import jodd.http.HttpRequest;
+import jodd.http.HttpResponse;
+
 public class CameraWebViewActivity extends AppCompatActivity {
 
     private static final Logger logger = Logger.getLogger(CameraWebViewActivity.class.getName());
+
+    private String ip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +28,7 @@ public class CameraWebViewActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String ipAddress = intent.getStringExtra(MainActivity.IP_ADDRESS);
         logger.info("Sent ip address from MainActivity: " + ipAddress);
+        ip = ipAddress;
 
         WebView webView = (WebView) findViewById(R.id.camera_view);
         String html = "<!DOCTYPE html>\n" +
@@ -46,10 +50,29 @@ public class CameraWebViewActivity extends AppCompatActivity {
 
     public void turnLeft(View view) {
         //TODO: send request "/left"
+        final HttpRequest httpRequest = HttpRequest.get("http://"+ip+":8089/sh/left/");
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                HttpResponse response = httpRequest.send();
+                System.out.println(response.toString());
+            }
+        };
+        new Thread(r).start();
+
     }
 
     public void turnRight(View view) {
         //TODO: send request "/right"
+        final HttpRequest httpRequest = HttpRequest.get("http://"+ip+":8089/sh/right/");
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                HttpResponse response = httpRequest.send();
+                System.out.println(response.toString());
+            }
+        };
+        new Thread(r).start();
     }
 
 }
