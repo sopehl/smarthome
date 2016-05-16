642524ffeb2b97e50bea4523c444d873e6ffe9a5
semihokanpehlivan <semihokanp@gmail.com>
2016-05-16 18:54:59 +0300

    Polishing

--- dht11_executable.py
+++ dht11_executable.py
@@ -13,13 +13,12 @@ GPIO.cleanup()
 instance = dht11.DHT11(pin=4)
 
 while True:
-	result = instance.read()
-	if result.is_valid():
-		temp = [str(result.temperature), str(datetime.datetime.now())]
-    		hum = [str(result.humidity), str(datetime.datetime.now())]
-    		tah.insert_hum(hum)
-    		tah.insert_tem(temp)
-		print("Last valid input: " + str(datetime.datetime.now()))
-		print("Temperature: %d C" % result.temperature)
-		print("Humidity: %d %%" % result.humidity)
-	
+    result = instance.read()
+    if result.is_valid():
+        temp = [str(result.temperature), str(datetime.datetime.now())]
+        hum = [str(result.humidity), str(datetime.datetime.now())]
+        tah.insert_hum(hum)
+        tah.insert_tem(temp)
+        print("Last valid input: " + str(datetime.datetime.now()))
+        print("Temperature: %d C" % result.temperature)
+        print("Humidity: %d %%" % result.humidity)
